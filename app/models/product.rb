8363d48ae2b91ae2eb57e414ec178f84ec31ba56
arif362 <gmarifulislamarif@gmail.com>
2017-03-13 19:01:25 +0600

    second commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   mount_uploader :image_url, ImageUrlUploader
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, :numericality => {greater_than_or_equal_to: 0.01}
+  validates :image_url, :format =>  {
+                :with => %r{\.(gif|jpg|png)}i,
+                :message =>  'Image must be gif or jpg or png.'
+                      }
 end
