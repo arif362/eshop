c0c8ea1c3eaebedc1b978e0d59b2c8c83fc7e39d
arif362 <gmarifulislamarif@gmail.com>
2017-03-14 19:48:11 +0600

    from office

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,7 @@
 class StoresController < ApplicationController
   def index
     @products=Product.all
+    @products=Product.paginate(:page => params[:page], :per_page => 2)
+
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,25 @@
 class Product < ActiveRecord::Base
+ # default_scope order: 'title'
+  has_many :line_items
+
   mount_uploader :image_url, ImageUrlUploader
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, :numericality => {greater_than_or_equal_to: 0.01}
-  validates :image_url, :format =>  {
-                :with => %r{\.(gif|jpg|png)}i,
-                :message =>  'Image must be gif or jpg or png.'
+  validates :image_url, :format => {
+                          :with => %r{\.(gif|jpg|png)}i,
+                          :message => 'Image must be gif or jpg or png.'
                       }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
